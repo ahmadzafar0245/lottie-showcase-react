31ded275de150fa7ff97a65ec6a933f484d52c78
Arafat <arafat.khan@aug-tech.com>
2023-10-31 13:30:59 +0800

    removed self

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-self.addEventListener("fetch", function(event) {
+addEventListener("fetch", function(event) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
         if (response) {
