104396fffd89d93a41c01ab72c06780c310997bc
Arafat <arafat.khan@aug-tech.com>
2023-10-31 13:44:54 +0800

    commented

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,18 +1,18 @@
-window.addEventListener("fetch", function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open().then(function(cache) {
-                cache.put(event.request.url, res.clone());
-                return res;
-              });
-            })
+// addEventListener("fetch", function(event) {
+//     event.respondWith(
+//       caches.match(event.request).then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           return fetch(event.request)
+//             .then(function(res) {
+//               return caches.open().then(function(cache) {
+//                 cache.put(event.request.url, res.clone());
+//                 return res;
+//               });
+//             })
            
-        }
-      })
-    );
-  });
+//         }
+//       })
+//     );
+//   });
